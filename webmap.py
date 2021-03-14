004e26f0a3e22b6bac0fa7660da212ccc6347ba1
Koose <KooseTheBlogger@gmail.com>
2021-03-14 08:19:32 -0700

    complete

--- webmap.py
+++ webmap.py
@@ -1,4 +1,14 @@
 import folium
-map = folium.Map(location=[7, 1.0232],
-                 zoom_start=7, titles="Stamen Terrain")
+map = folium.Map(location=[6.6998, -1.6246],
+                 zoom_start=20, titles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="Kumasi")
+
+for coordinates in [[6.6719, -1.6068], [6.6939, -1.6363]]:
+    map.add_child(folium.Marker(location=coordinates,
+                                popup="This is my Fav places in Kuamsi", icon=folium.Icon(color='blue')))
+
+
+map.add_child(fg)
+
 map.save("GhanaMap.html")
