90db8fd59a7d122c2142de8dce54825ffc102e39
Koose <KooseTheBlogger@gmail.com>
2021-04-29 06:04:57 +0000

    All done day13

--- webmap.py
+++ webmap.py
@@ -4,16 +4,33 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'pink'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 
 map = folium.Map(location=[37.0902, 95.7129],
-                 zoom_start=10, titles="Stamen Terrain")
+                 zoom_start=6, titles="Koose")
 
 fg = folium.FeatureGroup(name="Kumasi")
 
 
-for k, l in zip(lat, lon):
-    map.add_child(folium.Marker(location=[k, l],
-                                popup="This is my Fav places in Kuamsi", icon=folium.Icon(color='blue')))
+for k, l, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[
+                 k, l], radius=6, popup=str(el) + " This is my Fav places in Kuamsi", fill_color=color_producer(el), color='grey', fill_capacity=0.6))
 
 
 map.add_child(fg)
