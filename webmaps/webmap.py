b7ec46fd4de7a41ed4375d9c0cbae83692864557
BraKoose <72326008+BraKoose@users.noreply.github.com>
2021-04-29 05:46:06 +0000

    Add files via upload

--- webmaps/webmap.py
+++ webmaps/webmap.py
@@ -1,14 +1,36 @@
 import folium
-map = folium.Map(location=[6.6998, -1.6246],
-                 zoom_start=20, titles="Stamen Terrain")
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'pink'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
+map = folium.Map(location=[37.0902, 95.7129],
+                 zoom_start=6, titles="Koose")
 
 fg = folium.FeatureGroup(name="Kumasi")
 
 
-
-for coordinates in [[6.6719, -1.6068], [6.6939, -1.6363]]:
-    map.add_child(folium.Marker(location=coordinates,
-                                popup="This is my Fav places in Kuamsi", icon=folium.Icon(color='blue')))
+for k, l, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[
+                 k, l], popup=str(el) + " This is my Fav places in Kuamsi", icon=folium.Icon(color=color_producer(el))))
 
 
 map.add_child(fg)
